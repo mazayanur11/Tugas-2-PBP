be107b21a5a9e5aad41e49ead63a7bdd37009c3a
mazayanur11 <mazaya.nur11@ui.ac.id>
2022-09-13 10:19:02 +0700

    menambahkan Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_catalog,
+        'nama': 'Mazaya Nur Labiba',
+        'id': '2106639485',
+    }
+    return render(request, "katalog.html", context)
